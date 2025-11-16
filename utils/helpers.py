37e68e4cfc00ea932bad0c8ad7894e468052f0a1
prohamidullovra-sketch <prohamidullovra@gmail.com>
2025-11-16 10:05:02 +0300

    Create helpers.py

---
+++ utils/helpers.py
@@ -0,0 +1,11 @@
+import logging
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler('bot.log', encoding='utf-8'),
+            logging.StreamHandler()
+        ]
+    )
